5232e936fcdd91d2d23c925082f513839841e3ed
orestis.alagialoglou <orestis.alagialoglou@marinetraffic.com>
2023-06-05 21:17:21 +0300

    Add App main component with routing

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import Welcome from "./components/Welcome";
+import React from "react";
+import PairGame from "./components/PairGame";
+import Leaderboard from "./components/Leaderboard";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Welcome />}></Route>
+            <Route path="/pairGame" element={<PairGame />}></Route>
+            <Route path="/leaderboard" element={<Leaderboard />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
+  );
+}
+
+export default App;

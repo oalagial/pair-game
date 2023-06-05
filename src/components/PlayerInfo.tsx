31d7cac0d38783d07bf760a36e695ba137618a5a
orestis.alagialoglou <orestis.alagialoglou@marinetraffic.com>
2023-06-05 21:47:01 +0300

    Add propTypes in PlayerInfo component

--- src/components/PlayerInfo.tsx
+++ src/components/PlayerInfo.tsx
@@ -1,6 +1,10 @@
 import { useNavigate } from "react-router-dom";
+interface PlayerInfoProps {
+  active_nickname: string;
+  lifes: number;
+}
 
-function PlayerInfo({ active_nickname, lifes }) {
+function PlayerInfo({ active_nickname, lifes }: PlayerInfoProps) {
   const navigate = useNavigate();
 
   if (!active_nickname) {
